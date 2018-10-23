916ea90416dfd96493aa509502674d45eb3cee8d
Cesare <cesare.camurani@gmail.com>
2018-10-23 15:40:49 +0000

    Refactored with Methods

--- directory.rb
+++ directory.rb
@@ -13,13 +13,22 @@ students = [
 "Sauron",
 ]
 
-# and the print them
-puts "The students of Villains Academy:"
-puts "-----------------"
-students.each do |student|
-  puts student
+def print_header
+  puts "The students of Villains Academy:"
+  puts "-----------------"
 end
 
-# finally we print the total 
-puts "Overall, we have #{students.count} great students!"
+def print(names)
+  names.each do |name|
+    puts name
+  end
+end
 
+def print_footer(names)
+  puts "-----------------"
+  puts "Overall, we have #{names.count} great students!"
+end
+
+print_header
+print(students)
+print_footer(students)
