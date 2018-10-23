dff5f6509bc0f31cd81296771259a956ffc897ed
Cesare <cesare.camurani@gmail.com>
2018-10-23 17:07:02 +0000

    Exercise 3 commit

--- directory.rb
+++ directory.rb
@@ -22,7 +22,7 @@ end
 
 def print(students) 
   students.each.with_index(1) do |student, index|
-    if "#{student[:name]}".start_with? "C"
+    if "#{student[:name]}".length < 12
       puts "#{index}. #{student[:name]} (#{student[:cohort]} cohort)"
     end
   end
