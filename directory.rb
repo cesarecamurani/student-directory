887388215ae498a7bd0beca6aaf27d3589743e37
Cesare <cesare.camurani@gmail.com>
2018-10-24 11:31:31 +0000

    Exercise 4 commit

--- directory.rb
+++ directory.rb
@@ -19,16 +19,27 @@ def input_students
     age = gets.gsub("\n", "")
     puts "What's the Country?"
     country = gets.gsub("\n", "")
-    students << {name: name, age: age, country: country, cohort: :october}
-    puts "Now we have #{students.count} students"
+    puts "What's the cohort?"
+    cohort = gets.gsub("\n", "")
+    students << {name: name, age: age, country: country, cohort: cohort}
+    if students.count == 1
+      puts "Now we have 1 student"
+    else
+      puts "Now we have #{students.count} students"
+    end
     name = gets.gsub("\n", "")
   end
   students
 end
- 
+  
+
 def print(students) 
-  students.each.with_index(1) do |student, index|
-    puts "#{index}. #{student[:name]}, #{student[:age]}, #{student[:country]} (#{student[:cohort]} cohort)"
+  counter = 0
+  while counter < students.count
+    students.each.with_index(1) do |student, index|
+      puts "#{index}. #{student[:name]}, #{student[:age]}, #{student[:country]} (#{student[:cohort]} cohort)"
+      counter += 1
+    end
   end
 end
 
@@ -44,3 +55,4 @@ end
  print_header(students)
  print(students)
  print_footer(students)
+
