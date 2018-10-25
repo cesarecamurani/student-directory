cacc076d2f25f000a2a79dde4cad42594fc888db
Cesare <cesare.camurani@gmail.com>
2018-10-25 17:20:19 +0000

    directory exe 12

---
+++ directory_exe12.rb
@@ -0,0 +1,45 @@
+def print_header(students)
+  if !students.empty?
+    puts "The students of Villains Academy:"
+    puts "------------"
+  else
+  end
+end
+
+def input_students
+  puts "Please enter the names of the students: "
+  puts "To finish, just hit return twice"
+  
+  students = []
+    
+  name = gets.chomp
+  while !name.empty? do 
+    puts "What's the age?"
+    age = gets.chomp
+    puts "What's the Country?"
+    country = gets.chomp
+    students << {name: name, age: age, country: country, cohort: :october}
+    puts "Now we have #{students.count} students"
+    name = gets.chomp
+  end
+  students
+end
+
+def print(students) 
+  students.each.with_index(1) do |student, index|
+    puts "#{index}. #{student[:name]}, #{student[:age]}, #{student[:country]} (#{student[:cohort]} cohort)"
+  end
+end
+
+def print_footer(students)
+  if !students.empty?
+    puts "------------"
+    puts "Overall, we have #{students.count} great students!"
+  else
+  end
+end
+
+students = input_students
+print_header(students)
+print(students)
+print_footer(students)
